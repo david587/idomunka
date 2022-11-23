588765b8b33f29c2a408da19e2c585a9476664c8
David Barath <davidbarath08@gmail.com>
2022-11-23 23:42:37 +0100

    kesz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,12 @@
+// <!-- 
+// * File: app.component.ts
+// * Author: Baráth Dávid
+// * Copyright: 2022, Baráth Dávid
+// * Group: Szoft 2 N
+// * Date: 2022-11-23
+// * Github: https://github.com/david587/
+// * Licenc: GNU GPL -->
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +15,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'idomunka';
+  cim = 'Időmunka';
+  nev ="Baráth Dávid";
+  osztaly="Szoft 2 N"
+  datum="2022.11.23";
+
+  hossz !: number;
+  elotolas !: number;
+  fordulatszam !: number;
+  ido!: number;
+  show: boolean = false;
+
+  calcArea():void{
+    this.ido = this.hossz / this.elotolas * this.fordulatszam;
+    this.timeShow()
+  }
+
+  timeShow() :void {
+    this.show = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
+// <!-- 
+// * File: app.module.ts
+// * Author: Baráth Dávid
+// * Copyright: 2022, Baráth Dávid
+// * Group: Szoft 2 N
+// * Date: 2022-11-23
+// * Github: https://github.com/david587/
+// * Licenc: GNU GPL -->
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +17,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
